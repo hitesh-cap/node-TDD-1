85ef925bbb8c6bb580e58b486fb1b95f8f1faa7a
hitesh-cap <106952452+hitesh-cap@users.noreply.github.com>
2022-06-29 15:19:46 +0530

    Update firsttestcase.test.js

--- firsttestcase.test.js
+++ firsttestcase.test.js
@@ -1,1 +1,16 @@
 it.todo("jest setup");
+const calculator = require("./src/mapper/calculator");
+describe('calculator testing',()=>{
+    it("addition check",()=>{
+        expect(calculator('addition',1,2,3)).toBe(6);
+    })
+    it("subtraction check",()=>{
+        expect(calculator('subtraction',2,3)).toBe(-1);
+    })
+    it("multiplication check",()=>{
+        expect(calculator('multiplication',1,2,3)).toBe(6);
+    })
+    it("division check",()=>{
+        expect(calculator('division',1,2)).toBe(2);
+    })
+})
